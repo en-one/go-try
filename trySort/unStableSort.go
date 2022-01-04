cf24019ca4064f91f373cbe2c883f2a1df7dd98f
en-one <Ab960303>
2022-01-05 00:06:05 +0800

    update unStableSort

--- trySort/unStableSort.go
+++ trySort/unStableSort.go
@@ -2,7 +2,7 @@ package main
 
 // selectSort 选择--------------------------------------------------
 // 手里一把扑克牌，把最小的放在最左边，然后从下一张开始，接着找最小的。
-func selectSort1(arr []int) []int {
+func selectSort(arr []int) []int {
 	for i := 0; i < len(arr); i++ {
 		min := i
 		for j := i + 1; j < len(arr); j++ {
@@ -14,79 +14,6 @@ func selectSort1(arr []int) []int {
 	}
 
 	return arr
-}
-
-func selectSort(arr []int) []int {
-	var result []int
-	len := len(arr)
-	for i := 0; i < len; i++ {
-		smallest_index := findSmallest(arr)
-		result = append(result, arr[smallest_index])
-		arr = append(arr[:smallest_index], arr[smallest_index+1:]...)
-	}
-
-	return result
-}
-
-func findSmallest(arr []int) int {
-	if len(arr) == 0 {
-		return 0
-	}
-	smallest := arr[0]
-	smallest_index := 0
-	for i := 0; i < len(arr); i++ {
-		if arr[i] < smallest {
-			smallest = arr[i]
-			smallest_index = i
-		}
-	}
-
-	return smallest_index
-}
-
-// 堆排序---------------------------------------------
-// 基于堆的排序其实是一种选择排序
-func headSort(num []int) []int {
-	// 构建二叉堆
-	len := len(num)
-	last_node := len - 1
-	parent_last_node := last_node / 2
-	for i := parent_last_node; i >= 0; i-- {
-		heapify(num, len, i)
-	}
-	// 排序
-	// 把最大的放最后，然后重新建堆
-	for i := len - 1; i >= 0; i-- {
-		num[0], num[i] = num[i], num[0]
-		heapify(num, i, 0)
-	}
-
-	return num
-}
-
-// 小顶堆
-func heapify(tree []int, num, index int) []int {
-	if index >= num {
-		return tree
-	}
-	lchild := 2*index + 1
-	rchild := 2*index + 2
-	max := index
-
-	if lchild < num && tree[lchild] < tree[max] {
-		max = lchild
-	}
-
-	if rchild < num && tree[rchild] < tree[max] {
-		max = rchild
-	}
-
-	if max != index {
-		tree[index], tree[max] = tree[max], tree[index]
-		heapify(tree, num, max)
-	}
-	return tree
-
 }
 
 // 希尔排序---------------------------------------------
@@ -130,4 +57,49 @@ func quickSort(arr []int) []int {
 	left = quickSort(left)
 	right = quickSort(right)
 	return append(left, append([]int{pivot}, right...)...)
+}
+
+// 堆排序---------------------------------------------
+// 基于堆的排序其实是一种选择排序
+func headSort(arr []int) []int {
+	// 构建二叉堆
+	len := len(arr)
+	last_node := len - 1
+	last_node_parent := last_node / 2
+	for i := last_node_parent; i >= 0; i-- {
+		heapify(arr, len, i)
+	}
+	// 排序
+	// 把最大的放最后，然后重新建堆
+	for i := len - 1; i >= 0; i-- {
+		arr[0], arr[i] = arr[i], arr[0]
+		heapify(arr, i, 0)
+	}
+
+	return arr
+}
+
+// 大顶堆
+func heapify(tree []int, num, index int) []int {
+	if index >= num {
+		return tree
+	}
+	lchild := 2*index + 1
+	rchild := 2*index + 2
+	max := index
+
+	if lchild < num && tree[lchild] > tree[max] {
+		max = lchild
+	}
+
+	if rchild < num && tree[rchild] > tree[max] {
+		max = rchild
+	}
+
+	if max != index {
+		tree[index], tree[max] = tree[max], tree[index]
+		heapify(tree, num, max)
+	}
+	return tree
+
 }
